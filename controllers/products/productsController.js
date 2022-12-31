6442d9252a904d284501c5d783a5078c0c14daba
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2022-12-31 15:02:35 +0200

    v. 1.1.2

--- controllers/products/productsController.js
+++ controllers/products/productsController.js
@@ -4,23 +4,23 @@ const notAllowedProductsObj = require("./notAllowedProductsObj");
 const { createNotFoundError } = require("../../middlewares");
 
 const getDailyRateController = async (req, res) => {
-    const dailyRate = await calculateDailyRate(req.body);
-    const { notAllowedProducts, notAllowedProductsAll } = await notAllowedProductsObj(req.body.bloodType);
-    return res.status(200).json({ dailyRate, notAllowedProducts, notAllowedProductsAll, });
+  const dailyRate = await calculateDailyRate(req.body);
+  const { notAllowedProducts, notAllowedProductsAll } = await notAllowedProductsObj(req.body.bloodType);
+  return res.status(200).json({ dailyRate, notAllowedProducts, notAllowedProductsAll, });
 };
 
 const getDailyRateUserController = async (req, res) => {
-    const { user } = req;
-    const dailyRate = await calculateDailyRate(user.infouser);
-    const { notAllowedProducts, notAllowedProductsAll } = await notAllowedProductsObj(user.infouser.bloodType);
-    user.infouser = {
-        ...user.infouser,
-        dailyRate,
-        notAllowedProducts,
-        notAllowedProductsAll,
-    };
-    await User.findByIdAndUpdate(user._id, user);
-    return res.status(200).json({ data: user.infouser });
+  const { user } = req;
+  const dailyRate = await calculateDailyRate(user.infouser);
+  const { notAllowedProducts, notAllowedProductsAll } = await notAllowedProductsObj(user.infouser.bloodType);
+  user.infouser = {
+    ...user.infouser,
+    dailyRate,
+    notAllowedProducts,
+    notAllowedProductsAll,
+  };
+  await User.findByIdAndUpdate(user._id, user);
+  return res.status(200).json({ data: user.infouser });
 };
 
 const getAllProductsByQuery = async (req, res, next) => {
@@ -37,17 +37,16 @@ const getAllProductsByQuery = async (req, res, next) => {
         { 'title.ua': { $regex: titleFromUrl, $options: 'i' } },
       ],
     }).limit(limit);
-
     if (newProducts.length === 0) {
       return next(createNotFoundError());
-    }
+    };
     return res.status(200).json({ data: newProducts });
-  }
+  };
   return res.status(200).json({ data: products });
 };
 
 module.exports = {
-    getDailyRateController,
-    getDailyRateUserController,
-    getAllProductsByQuery,
+  getDailyRateController,
+  getDailyRateUserController,
+  getAllProductsByQuery,
 };
