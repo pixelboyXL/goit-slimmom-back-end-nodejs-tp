1af294246a46252028fb0baeea980bf46455f4bf
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2023-01-06 16:53:18 +0200

    v. 1.1.7

--- server.js
+++ server.js
@@ -4,18 +4,16 @@ const app = require('./app');
 
 const {HOST_DB, PORT} = process.env;
 
-    const main = async () => {
+const main = async () => {
   try {
     if (!HOST_DB) {
       throw new Error("HOST_DB not set!");
     };
-
     await mongoose.connect(HOST_DB);
     console.log("Database connection successful");
     app.listen(PORT, (err) => {
       if (err) throw err;
       console.log(`Server is listening on port: ${PORT}`);
-
     });
   } catch (error) {
     console.error("Error:", error.message);
